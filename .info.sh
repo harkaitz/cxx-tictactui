2cbfc2102d23a1b8806d482b6013a0abcc11de7b
Harkaitz Agirre Ezama <harkaitzv@gmail.com>
2023-05-29 23:37:09 +0200

    m

--- .info.sh
+++
@@ -1,5 +0,0 @@
-## -- AUTO --
-auto() {
-    auto-cxx-mit "$@"
-}
-## -- AUTO --
--- main.cc
+++ main.cc
@@ -3,8 +3,8 @@
 #include "minimax.hh"
 
 #define COPYRIGHT_LINE \
-    "Bug reports, feature requests to gemini|https://harkadev.com/oss" "\n" \
-    "Copyright (c) 2022 Harkaitz Agirre, harkaitz.aguirre@gmail.com" "\n" \
+    "Bug reports, feature requests to https://harkadev.com/oss" "\n" \
+    "Copyright (c) 2023 Harkaitz Agirre, harkaitz.aguirre@gmail.com" "\n" \
     ""
 
 static char const help[] =
